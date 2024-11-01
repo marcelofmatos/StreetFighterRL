92e0f63ec6f9c94bc3891b218d639d5ec5aeaec2
Marcelo Matos <marcelofmatos@gmail.com>
2024-11-01 14:19:01 -0400

    fix: model vs joystick player

---
+++ prepare.sh
@@ -0,0 +1,6 @@
+#sudo apt-get install libsdl2-dev zlib1g-dev
+#sudo apt-get install gcc-10 g++-10
+#export CC=/usr/bin/gcc-10
+#export CXX=/usr/bin/g++-10
+pip install --upgrade pip
+pip install -r requirements.txt
